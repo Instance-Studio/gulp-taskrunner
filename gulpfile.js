d575ae09f64efe65d25a07ad344f3c0a7aaf4818
Joren Rothman <rothmanjoren@gmail.com>
2019-02-12 23:54:50 +0100

    Added feature for multi tasks :sparkles:

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,13 @@ const addTask = (name, taskFile) => {
     gulp.task(name, require(`./src/config/tasks/${taskFile}`)(gulp, plugins, settings));
 }
 
+const addMultiTask = (name, tasks) => {
+    gulp.task(name, gulp.series(tasks));
+}
+
 addTask('compile:js', 'compile-js');
 addTask('compile:scss', 'compile-scss');
+addMultiTask('compile:all', ['compile:js', 'compile:scss']);
 
 // gulp.task('compile:js', require('./src/config/tasks/compile-js')(gulp, plugins, settings));
 // gulp.task('compile:scss', require('./src/config/tasks/compile-scss')(gulp, plugins, settings));
