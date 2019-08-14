1381292989d4ce2a6d214e4082874e08a4cc2cb2
Joren Rothman <rothmanjoren@gmail.com>
2019-08-14 10:16:47 +0200

    removed assets

--- src/js/site.js
+++
@@ -1,8 +0,0 @@
-const jQuery = require('jquery');
-var Theme = require('./modules/theme');
-//var Sliders = require('./modules/sliders');
-
-jQuery(document).ready(function($) { // eslint-disable-line no-unused-vars
-	Theme.init();
-	//Sliders.init();
-});
